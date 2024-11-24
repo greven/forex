2fd28d5b04be4742a60d17f4afa7c87cc864fabd
Nuno Freire <mail@nuno.site>
2024-11-24 14:15:27 +0000

    chore: git_ops config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,11 @@
 import Config
 
+config :git_ops,
+  mix_project: Mix.Project.get!(),
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/greven/forex.git",
+  manage_mix_version?: true,
+  manage_readme_version: "README.md",
+  version_tag_prefix: "v"
+
 import_config "#{Mix.env()}.exs"
